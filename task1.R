d25600429b04c858a0d69f19fc2abf78dad1bcb3
ReshmaKanna-22 <153736988+ReshmaKanna-22@users.noreply.github.com>
2024-07-21 21:12:22 +0530

    Update task1.R

--- task1.R
+++ task1.R
@@ -1,5 +1,6 @@
 data=women
 data
 chart1=barplot(data$height,col="red",main="Bar Chart",xlab="Height",ylab="Weight")
+chart1
 chart2=hist(data$weight,col ="blue",main="Histogram",xlab="Height",ylab="Weight")
 chart2
