78042a0ca2cfc1001529302b133fdabe25acf0d5
ReshmaKanna-22 <153736988+ReshmaKanna-22@users.noreply.github.com>
2024-07-21 21:22:50 +0530

    Delete task1.R

--- task1.R
+++
@@ -1,6 +0,0 @@
-data=women
-data
-chart1=barplot(data$height,col="red",main="Bar Chart",xlab="Height",ylab="Weight")
-chart1
-chart2=hist(data$weight,col ="blue",main="Histogram",xlab="Height",ylab="Weight")
-chart2
