e0824d62859c6481505d3a7a03d5fe2c2613a281
ReshmaKanna-22 <153736988+ReshmaKanna-22@users.noreply.github.com>
2024-07-21 21:09:47 +0530

    Add files via upload

---
+++ task1.R
@@ -0,0 +1,5 @@
+data=women
+data
+chart1=barplot(data$height,col="red",main="Bar Chart",xlab="Height",ylab="Weight")
+chart2=hist(data$weight,col ="blue",main="Histogram",xlab="Height",ylab="Weight")
+chart2
